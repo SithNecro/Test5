21546386d73d67bcad6295929d72976b43a63ae9
necrofer <jfmg1188@gmail.com>
2024-10-25 11:59:06 +0200

    z

--- Objetos_Legendarios.js
+++ Objetos_Legendarios.js
@@ -56,5 +56,5 @@ function cargarTesoroLegendario() {
 
  // Función para barajar y poner la imagen de trasera del tesoro
         function barajarTesoros() {
-          	document.getElementById('imagen-tesoro').src = 'img/Trasera legendarios.png';
+          	document.getElementById('imagen-tesoro').src = 'img/traseras/Trasera legendarios.png';
         }
