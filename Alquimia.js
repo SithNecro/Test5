79e4a0d26e66701350c7ebe5013e6f1481652850
necrofer <jfmg1188@gmail.com>
2024-11-22 00:52:56 +0100

    Update Alquimia.js

--- Alquimia.js
+++ Alquimia.js
@@ -189,12 +189,12 @@ function initializeMaterialDropdown() {
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Cargando datos de LocalStorage...");
     let defaultRecipes = [
-        { type: "basic", name: "Poción de Curación", ingredients: ["Sangre humana", "Cola de rata", "Jengibre ceniciento"], default: true },
-        { type: "basic", name: "Poción contra Enfermedades", ingredients: ["Piel de zombi", "Ala de murciélago", "Laurel del monje"], default: true },
-        { type: "basic", name: "Bomba Flamígera", ingredients: ["Corazón de bestia", "Cola de rata", "Baya lunar"], default: true },
-        { type: "basic", name: "Antídoto", ingredients: ["Colmillo de araña", "Barbárea", "Agracejo"], default: true },
-        { type: "basic", name: "Frasco de Experiencia", ingredients: ["Sangre de dragón", "Hiedra dulce", "Belladona"], default: true },
-        { type: "basic", name: "Poción de Restauración", ingredients: ["Sangre de vampiro", "Sangre de troll", "Corteza de arce rojo"], default: true }
+        { type: "basic", name: "Poción de Curación", ingredients: ["Sangre humana", "Cola de rata", "Jengibre ceniciento"], default: true,title:"Sana Sanita" },
+        { type: "basic", name: "Poción contra Enfermedades", ingredients: ["Piel de zombi", "Ala de murciélago", "Laurel del monje"], default: true ,title:"Sana enferme"},
+        { type: "basic", name: "Bomba Flamígera", ingredients: ["Corazón de bestia", "Cola de rata", "Baya lunar"], default: true, title: "pum" },
+        { type: "basic", name: "Antídoto", ingredients: ["Colmillo de araña", "Barbárea", "Agracejo"], default: true, title: "cura eneno" },
+        { type: "basic", name: "Frasco de Experiencia", ingredients: ["Sangre de dragón", "Hiedra dulce", "Belladona"], default: true, title: "Exp up" },
+        { type: "basic", name: "Poción de Restauración", ingredients: ["Sangre de vampiro", "Sangre de troll", "Corteza de arce rojo"], default: true, title: "restaura tripita" }
     ];
     let storedRecipes = JSON.parse(localStorage.getItem(RECIPES_KEY)) || [];
 
