b8c02df54a382ea253faf5c5cfe56892be8f3183
necrofer <jfmg1188@gmail.com>
2024-11-28 20:29:48 +0100

    Update controlpersonajes.js

--- controlpersonajes.js
+++ controlpersonajes.js
@@ -48,7 +48,7 @@ function renderTable() {
         // Nombre del personaje
         const nameCell = document.createElement('td');
         nameCell.innerHTML = `
-        <input type="text" class="input-field" value="${character.name}" onchange="updateName(${index}, this.value)">;
+        <input type="text" class="input-field" value="${character.name}" onchange="updateName(${index}, this.value)"><br></br>
         <input type="text" id="exp${index}" class="input-field">`;
         row.appendChild(nameCell);
 
